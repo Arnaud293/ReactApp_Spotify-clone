2167af473b9982e017517d55805d718d74d93775
Arnaud293 <arnaudlt293@gmail.com>
2022-12-17 12:00:03 +0100

    get props and preparing function to manage player

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -4,9 +4,15 @@ import { useDispatch } from "react-redux";
 import PlayPause from "./PlayPause";
 import { playPause, setActiveSong } from "../redux/features/playerSlice";
 
-const SongCard = ({song, index}) => {
+const SongCard = ({song,  isPlaying, activeSong, index, data}) => {
 
-  const activeSong = 'Test';
+
+  const handlePauseClick = () => {
+
+  }
+  const handlePlayClick = () => {
+
+  }
   
   return (
   <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
@@ -14,7 +20,13 @@ const SongCard = ({song, index}) => {
           <div 
           className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex
            ${activeSong?.title === song.title ? 'flex bg-black bg-opacity-70 ' : 'hidden'}`}>
-            <PlayPause />
+            <PlayPause 
+              song={song} 
+              handlePause={handlePauseClick} 
+              handlePlay={handlePlayClick}
+              isPlaying={isPlaying}
+              activeSong={activeSong}
+              />
           </div>
           <img src={song.images?.coverart} alt="song-image" />
       </div>
