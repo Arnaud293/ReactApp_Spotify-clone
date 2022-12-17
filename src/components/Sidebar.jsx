15a6be3e62f6a37e4dcdb396294f670c053ad2fe
Arnaud293 <arnaudlt293@gmail.com>
2022-12-17 18:38:29 +0100

    creating responsive version for sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import {RiCloseLine} from 'react-icons/ri';
+import { HiOutlineMenu } from "react-icons/hi";
 
 import Logo from '../../src/assets/logo1.png';
 import {links} from '../assets/constants';
@@ -18,13 +19,24 @@ const NavLinks = ({handleClick}) => (
 
 const Sidebar = () => {
 
-  const [mobileMenu, setMobileMenu] = useState(false);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   
   return (
   <>
     <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#20201f]">
       <img src={Logo} alt="logo" className="w-full h-44 object-contain" />
       <NavLinks />
+    </div>
+    <div className="absolute md:hidden block top-6 right-3">
+        {mobileMenuOpen ? (
+          <RiCloseLine onClick={() => setMobileMenuOpen(false)} className="w-6 h-6 text-white mr-2" />
+        ): (
+          <HiOutlineMenu onClick={() => setMobileMenuOpen(true)} className="w-6 h-6 text-white mr-2" />
+        )}
+    </div>
+    <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#20201f] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
+      <img src={Logo} alt="logo" className="w-full h-44 object-contain" />
+      <NavLinks onClick={() => setMobileMenuOpen(false)} />
     </div>
   </>
 )
