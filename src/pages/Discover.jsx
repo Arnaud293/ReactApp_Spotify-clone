bd6f08747ea984d375737dc4b0d339a1b4ea156f
Arnaud293 <arnaudlt293@gmail.com>
2022-12-17 11:59:31 +0100

    call activesong and isPlaying feature from redux, pass them to child

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,9 +1,15 @@
+import { useDispatch, useSelector } from 'react-redux';
 import {Error, Loader, SongCard} from '../components';
 import {genres} from '../assets/constants';
 
 import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 const Discover = () => {
+
+
+    const dispatch = useDispatch();
+    const {activeSong, isPlaying} = useSelector((state) => state.player);
+
     const {data, isFetching, error} = useGetTopChartsQuery();
 
     if (isFetching) return <Loader title="Loadings songs" />;
@@ -20,7 +26,7 @@ const Discover = () => {
             </div>
             <div className="flex flex-wrap sm:justify-start justify-center gap-8">
                 {data?.map((song, index) => (
-                    <SongCard key={song.key} song={song} index={index}/>
+                    <SongCard key={song.key} song={song} index={index} isPlaying={isPlaying} activeSong={activeSong} data={data}/>
                 ))}
             </div>
         </div>
