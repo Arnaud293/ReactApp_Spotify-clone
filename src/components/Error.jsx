44b291a0a7f9bf2dcf41909d998a18ad0586a941
Arnaud293 <arnaudlt293@gmail.com>
2022-12-17 10:55:57 +0100

    create content for loader and error

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
-const Error = () => (
-  <div>Error</div>
+const Error = ({}) => (
+  <div className='w-full flex justify-center items-center'>
+    <h1 className='font-bold text-2x1 text-white mt-2'>An error is occured please try again</h1>
+  </div>
 );
 
 export default Error;
