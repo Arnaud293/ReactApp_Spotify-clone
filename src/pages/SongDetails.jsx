a271753be9876ad9642b6b8d2d8e2541f8376725
Arnaud293 <arnaudlt293@gmail.com>
2022-12-18 13:06:12 +0100

    get related songs and get functions play/pause, render actually still empty

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { DetailsHeader, Error, Loader, RelatedSongs } from "../components";
 
 import { setActiveSong, playPause } from "../redux/features/playerSlice";
-import { useGetSongDetailsQuery } from "../redux/services/shazamCore";
+import { useGetSongDetailsQuery, useGetSongRelatedQuery } from "../redux/services/shazamCore";
 
 const SongDetails = () =>{
 
@@ -12,6 +12,18 @@ const SongDetails = () =>{
     const {activeSong, isPlaying} = useSelector((state) => state.player);
 
     const {data: songData, isFetching: isFetchingSongDetails} = useGetSongDetailsQuery({songid});
+    const {data, isFetching: isFetchingRelatedSongs, error} = useGetSongRelatedQuery({songid})
+
+    const handlePauseClick = () => {
+        dispatch(playPause(false));
+    }
+      const handlePlayClick = (song, index) => {
+        dispatch(setActiveSong({song, data, index}));
+        dispatch(playPause(true))
+    }
+
+    if(isFetchingSongDetails || isFetchingRelatedSongs) return <Loader title='Searching songs details'/>;
+    if(error) return <Error />
     
     return (
         <div className="flex flex-col">
@@ -28,6 +40,7 @@ const SongDetails = () =>{
                     }
                 </div>
             </div>
+            <RelatedSongs data={data} isPlaying={isPlaying} activeSong={activeSong} handlePauseClick={handlePauseClick} handlePlayClick={handlePlayClick} />
         </div>
     )
 } 
